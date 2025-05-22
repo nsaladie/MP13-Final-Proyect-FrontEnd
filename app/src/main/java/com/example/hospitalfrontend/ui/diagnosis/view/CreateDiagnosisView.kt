bfdf44c8f846f1a9bdc0fc4a23bb78c4efc467b2
Dafnert <dafne.rt04@gmail.com>
2025-05-22 21:33:17 +0200

    the #122 redesign the create medication screen

--- app/src/main/java/com/example/hospitalfrontend/ui/diagnosis/view/CreateDiagnosisView.kt
+++ app/src/main/java/com/example/hospitalfrontend/ui/diagnosis/view/CreateDiagnosisView.kt
@@ -33,7 +33,6 @@ import com.example.hospitalfrontend.domain.model.patient.PatientState
 import com.example.hospitalfrontend.data.remote.viewmodel.DiagnosisRemoteViewModel
 import com.example.hospitalfrontend.data.remote.response.RemoteApiMessageDiagnosis
 import com.example.hospitalfrontend.ui.auxiliary.viewmodel.AuxiliaryViewModel
-import com.example.hospitalfrontend.ui.medication.view.ItemInfo
 
 
 @OptIn(ExperimentalMaterial3Api::class)
